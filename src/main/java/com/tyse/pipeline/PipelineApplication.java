3f1c9303a2978a9167a949a7faa8d5c661e5b3dd
Manuel Motta <manuelmottaherrera@gmail.com>
2023-07-26 04:16:38 -0500

    entities

--- src/main/java/com/tyse/pipeline/PipelineApplication.java
+++ src/main/java/com/tyse/pipeline/PipelineApplication.java
@@ -1,12 +1,8 @@
 package com.tyse.pipeline;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseDataSource;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -17,10 +13,4 @@ public class PipelineApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PipelineApplication.class, args);
 	}
-
-//	@Bean
-//	@LiquibaseDataSource
-//	public DataSource liquibaseDataSource(DataSource dataSource) {
-//		return dataSource;
-//	}
 }
---
+++ src/main/java/com/tyse/pipeline/domain/entities/Dmp.java
@@ -0,0 +1,43 @@
+package com.tyse.pipeline.domain.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "DMP")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Dmp implements Serializable{
+
+	private static final long serialVersionUID = -7823186460173679406L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Short id;
+	
+	@Column
+	private byte[] dmp;
+
+	@Column
+    private Instant date;
+
+	@Column
+    private Long idUsers;
+
+	@Column
+    private String status;
+}
