48defbc736893c10b27dd061a0539a9da0603a19
Manuel Motta <manuelmottaherrera@gmail.com>
2023-10-06 18:42:26 -0500

    hilos en 50

--- src/main/java/com/tyse/pipeline/config/AsyncConfig.java
+++ src/main/java/com/tyse/pipeline/config/AsyncConfig.java
@@ -16,9 +16,9 @@ public class AsyncConfig implements AsyncConfigurer {
 	@Override
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);  // número inicial de hilos
-        executor.setMaxPoolSize(64);  // número máximo de hilos
-        executor.setQueueCapacity(100);  // capacidad de la cola de espera
+        executor.setCorePoolSize(20);  // número inicial de hilos
+        executor.setMaxPoolSize(50);  // número máximo de hilos
+        executor.setQueueCapacity(200);  // capacidad de la cola de espera
         executor.setThreadNamePrefix("MyAsyncThread-");  // prefijo para los nombres de hilos
         executor.initialize();
         return executor;
